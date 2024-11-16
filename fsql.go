7a593a1108ceb9444d31c543a72157079cc0c6fe
Gasquez Florian <m@fy.to>
2024-11-16 08:00:27 +0100

    Update fsql.go

--- fsql.go
+++ fsql.go
@@ -4,14 +4,16 @@ package fsql
 import (
 	"log"
 
+	"math/rand"
+
 	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/stdlib"
-
 	"github.com/jmoiron/sqlx" // SQL library
 	_ "github.com/lib/pq"     // PostgreSQL driver
 )
 
 var Db *sqlx.DB
+var readReplicasDbs []*sqlx.DB
 
 func PgxCreateDB(uri string) (*sqlx.DB, error) {
 	connConfig, _ := pgx.ParseConfig(uri)
@@ -26,13 +28,23 @@ func InitDB(database string) {
 	if err != nil {
 		log.Fatalf("Failed to connect to database: %v", err)
 	}
-
-	// Set reasonable limits
-	/*
-		Db.SetMaxOpenConns(100)
-		Db.SetMaxIdleConns(50)
-		Db.SetConnMaxLifetime(60 * time.Minute)
-	*/
+}
+func InitDbReplicas(databases []string) {
+	for _, dbURI := range databases {
+		replicaDb, err := PgxCreateDB(dbURI)
+		if err != nil {
+			log.Printf("Failed to connect to replica database %s: %v", dbURI, err)
+			continue
+		}
+		readReplicasDbs = append(readReplicasDbs, replicaDb)
+	}
+}
+func GetReplika() *sqlx.DB {
+	if len(readReplicasDbs) == 0 {
+		return Db
+	}
+	idx := rand.Intn(len(readReplicasDbs))
+	return readReplicasDbs[idx]
 }
 
 // CloseDB closes the database connection
@@ -40,6 +52,15 @@ func CloseDB() {
 	if Db != nil {
 		if err := Db.Close(); err != nil {
 			log.Printf("Error closing database: %v", err)
+		}
+	}
+}
+func CloseReplicas() {
+	for _, db := range readReplicasDbs {
+		if db != nil {
+			if err := db.Close(); err != nil {
+				log.Printf("Error closing replica database: %v", err)
+			}
 		}
 	}
 }
